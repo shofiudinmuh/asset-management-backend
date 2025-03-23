8bb8daa8dae4fcfb1e21e996dbb047d87902e41b
shofiudinmuh <shofiudinmuh@gmail.com>
2025-03-23 13:03:45 +0700

    update : fix database model

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Location;
+use App\Models\Maintenance;
+use App\Models\Transaction;
+use App\Models\AssetDocument;
 use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
@@ -17,4 +21,49 @@ class Asset extends Model
         'status',
         'location_id'
     ];
+
+    protected $casts = [
+        'purchase_date' => 'date',
+        'warranty_expiry' => 'date',
+    ];
+    
+    /**
+     * Get the location that owns the Asset
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    /**
+     * Get all of the maintenances for the Asset
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function maintenances(): HasMany
+    {
+        return $this->hasMany(Maintenance::class);
+    }
+
+    /**
+     * Get all of the transactions for the Asset
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    /**
+     * Get all of the assetDocuments for the Asset
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function assetDocuments(): HasMany
+    {
+        return $this->hasMany(AssetDocument::class);
+    }
 }
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Transaction;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\DatAssetsloquent\Relations\HasMany;
 
 class Location extends Model
 {
@@ -12,4 +14,24 @@ class Location extends Model
         'name',
         'address'
     ];
+
+    /**
+     * Get all of the assets for the Location
+     *
+     * @return HasMany
+     */
+    public function assets(): HasMany
+    {
+        return $this->hasMany(Assets::class);
+    }
+
+    /**
+     * Get all of the transactions for the Location
+     *
+     * @return \Illuminate\Database\Eloquent\Transactions\HasMany
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
